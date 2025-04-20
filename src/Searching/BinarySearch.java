f082a018ef4acfdb8f8a6fde0cf5ef7241ecc26a
shahriar808 <shasan5525@gmail.com>
2025-04-20 21:42:59 +0600

    implimenting linear & binary search

---
+++ src/Searching/BinarySearch.java
@@ -0,0 +1,28 @@
+package Searching;
+
+public class BinarySearch {
+    public static int binarySearch(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        int result = binarySearch(nums, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
---
+++ src/Searching/LinearSearch.java
@@ -0,0 +1,23 @@
+package Searching;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 12, 6, 15, 8, 19, 10};
+        int target = 11;
+        int result = linearSearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
