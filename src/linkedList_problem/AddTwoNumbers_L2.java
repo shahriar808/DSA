9095dd14f6b3b858fe850ef84199e0214add98b8
shahriar808 <shasan5525@gmail.com>
2025-05-01 20:11:52 +0600

    leetcode 2

---
+++ src/linkedList_problem/AddTwoNumbers_L2.java
@@ -0,0 +1,51 @@
+package linkedList_problem;
+
+public class AddTwoNumbers_L2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resHead = new ListNode(0);
+        ListNode p = resHead;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            p.next = new ListNode(sum % 10);
+            p = p.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return resHead.next;
+    }
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        AddTwoNumbers_L2 solution = new AddTwoNumbers_L2();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+// Definition for singly-linked list.
+class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
