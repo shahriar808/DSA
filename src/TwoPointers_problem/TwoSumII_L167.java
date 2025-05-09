9a6ff9b81ad51d4bdb27a0656d27357db4854758
shahriar808 <shasan5525@gmail.com>
2025-05-09 21:04:52 +0600

    leetcode 167

--- src/TwoPointers_problem/TwoSumII_L167.java
+++ src/TwoPointers_problem/TwoSumII_L167.java
@@ -2,19 +2,19 @@ package TwoPointers_problem;
 
 public class TwoSumII_L167 {
     public int[] twoSum(int[] numbers, int target) {
-        int low=0;
-        int high=numbers.length-1;
-        while (low<high){
-            int sum=numbers[low]+numbers[high];
-            if (sum>target){
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum > target) {
                 high--;
-            }
-            else  if (sum<target) low++;
-            else return new int[]{low+1,high+1};
+            } else if (sum < target) low++;
+            else return new int[]{low + 1, high + 1};
         }
         return new int[]{-1, -1};
 
     }
+
     public static void main(String[] args) {
         TwoSumII_L167 solution = new TwoSumII_L167();
         int[] numbers = {2, 7, 11, 15};
