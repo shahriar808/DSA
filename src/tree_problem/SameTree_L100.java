d66f8fc6ab60161fd5e18918992c07dab3cdc1db
shahriar808 <shasan5525@gmail.com>
2025-05-22 19:51:00 +0600

    leetcode 100

---
+++ src/tree_problem/SameTree_L100.java
@@ -0,0 +1,36 @@
+package tree_problem;
+
+public class SameTree_L100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val!=q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+    public static void main(String[] args) {
+        SameTree_L100 solution = new SameTree_L100();
+        TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        boolean result = solution.isSameTree(p, q);
+        System.out.println("Are the two trees the same? " + result);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
